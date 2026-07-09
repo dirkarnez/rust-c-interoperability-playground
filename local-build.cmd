197035f6d1db399beb9c49405a211fb2716f7654
Dirk Arnez <smalldirkalex@gmail.com>
2026-07-09 10:57:13 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -4,3 +4,5 @@ cargo install --force cbindgen
 cbindgen --help
 cbindgen --lang c --config cbindgen.toml --crate my_rust_library --output my_header.h
 cargo build --release
+
+gcc main.c -L./target/release -lrusty -lpthread -ldl -lm -o main
