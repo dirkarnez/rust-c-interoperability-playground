d2a4fbc0bde47dfa2ae3e8e740ace5ab5ee6aee8
dirkarnez <smalldirkalex@gmail.com>
2026-08-14 08:58:19 +0000

    - working

--- app/src/main.c
+++ app/src/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include "my_rust_lib.h"
+#include "rusty.h"
 
 int main() {
-    int32_t result = sum(10, 20);
+    int32_t result = sum(10, 24);
     printf("Result from Rust: %d\n", result);
     return 0;
 }
